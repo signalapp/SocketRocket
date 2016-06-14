676f8f9b55d058ae91d400abbe7b235d127b6997
Michael Kirk <michael.john.kirk@gmail.com>
2016-06-14 09:23:55 -0700

    nullable annotations

--- SocketRocket/NSURLRequest+SRWebSocket.h
+++ SocketRocket/NSURLRequest+SRWebSocket.h
@@ -16,13 +16,13 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface NSURLRequest (SRWebSocket)
 
-@property (nonatomic, retain, readonly) id<SRSecurityPolicy> SR_securityPolicy;
+@property (nullable, nonatomic, retain, readonly) id<SRSecurityPolicy> SR_securityPolicy;
 
 @end
 
 @interface NSMutableURLRequest (SRWebSocket)
 
-@property (nonatomic, retain) id<SRSecurityPolicy> SR_securityPolicy;
+@property (nullable, nonatomic, retain) id<SRSecurityPolicy> SR_securityPolicy;
 
 @end
 
